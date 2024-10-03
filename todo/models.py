a60468712dc0f56b45c07d7893326658c1a11126
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-03 12:59:10 -0400

    created todo modle and superuser, user = caleb, password = password

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """Todo model class"""
+
+    name = models.CharField(max_length=150)
+    complete_by_datetime = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name[50]
