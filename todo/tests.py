7c9bb7c830ea95817070ccbd3a4c93f288eeb758
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-03 22:05:39 -0400

    created test for the modle page

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from .models import Todo
+
+
 # Create your tests here.
+
+
+class BlogTest(TestCase):
+    """Blog Tests"""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username="testsuer", email="test@email.com", password="secret")
+
+        cls.todo = Todo.objects.create(name="bob", complete_by_datetime="10/9/2024")
+
+    def test_post_model(self):
+        self.assertEqual
